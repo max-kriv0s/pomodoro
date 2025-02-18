8839b94d20de50be5d21178b26abf7c8aa0937b7
max-kriv0s <116222442+max-kriv0s@users.noreply.github.com>
2025-02-18 23:06:28 +0300

    Refactoring/settings (#1)
    * add db&cache values to settings
    
    * fix database & cache accessor

--- settings.py
+++ settings.py
@@ -2,4 +2,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    sqlite_db_name: str = 'pomodoro.sqlite'
+    DB_HOST: str = '0.0.0.0'
+    DB_PORT: int = 5432
+    DB_USER: str = 'postgres'
+    DB_PASSWORD: str = 'password'
+    DB_NAME: str = 'pomodoro'
+    DB_DRIVER: str = 'postgresql+psycopg2'
+    
+    CACHE_HOST: str = '0.0.0.0'
+    CACHE_PORT: int = 6379
+    CACHE_DB: int = 0
+    
+    @property
+    def db_url(self):
+        return f'{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
