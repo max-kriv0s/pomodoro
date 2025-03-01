097d7ca9dd5109103eed75e64dbe7d149678642a
Maxim Krivoshein <max.kriv0s@gmail.com>
2025-03-01 18:34:38 +0300

    add google and yandex oauth

--- settings.py
+++ settings.py
@@ -17,6 +17,24 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = "secret_key"
     JWT_ALGORITHM: str = "HS256"
 
+    GOOGLE_CLIENT_ID: str = ""
+    GOOGLE_CLIENT_SECRET: str = ""
+    GOOGLE_REDIRECT_URL: str = ""
+    GOOGLE_TOKEN_URL: str = "https://accounts.google.com/o/oauth2/token"
+
+    YANDEX_CLIENT_ID: str = ""
+    YANDEX_CLIENT_SECRET: str = ""
+    YANDEX_REDIRECT_URL: str = ""
+    YANDEX_TOKEN_URL: str = "https://oauth.yandex.ru/token"
+
     @property
     def db_url(self):
         return f"{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def google_redirect_url(self):
+        return f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={self.GOOGLE_CLIENT_ID}&redirect_url={self.GOOGLE_REDIRECT_URL}&scope=openid%20profile%20email&access_type=offline"
+
+    @property
+    def yandex_redirect_url(self):
+        return f"https://oauth.yandex.ru/authorize?response_type=code&client_id={self.YANDEX_CLIENT_ID}&redirect_url={self.YANDEX_REDIRECT_URL}"
